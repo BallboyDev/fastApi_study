99cb4408297c5c2528d2f7da3899e9810daa8899
ballboyDev <yswgood0329@gmail.com>
2023-12-09 16:48:34 +0900

    db 연결

--- readme.md
+++ readme.md
@@ -10,12 +10,3 @@
 
 - svelte template 생성
 `npm create vite@latest frontend -- --template svelte`
-
-- SQLAlchemy ORM 설치
-`pip install sqlalchemy`
-
-- alembic 설치
-    - SQLAlchemy로 작성한 모델을 기반으로 데이터베이스를 관리 지원
-`pip install alembic`
-`alembic init migrations`
-`alembic revision --autogenerate`
--- study02/domain/question/question_router.py
+++ study02/domain/question/question_router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 # from database import SessionLocal
 from database import get_db
@@ -10,14 +11,21 @@ router = APIRouter(
 
 @router.get('/list')
 def question_list():
+    # 기본적인 DB호출 방법
     # db = SessionLocal()
     # _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
 
     # # db 세션 객체를 생성한 후에 db.close()를 수행하지 않으면 SQLAlchemy가 사용하는 커넥션 풀에 DB세션이 반환되지 않아 문제가 생긴다.
     # db.close()
 
+    # 의존성 주입 / 데이터 베이스 세션의 반환의 자동화
     with get_db() as db:
         _question_list = db.query(Question).order_by(Question.create_date.desc())
     all()    
 
+    return _question_list
+
+@router.get('/list2')
+def question_list(db: Session = Depends(get_db)):
+    _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
     return _question_list
